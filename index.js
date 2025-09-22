f59cf61dc7fbccee55587b60d4e9c54c3dd802b4
manumanuameeen <jerrykerry3806@gmail.com>
2025-09-22 08:32:13 +0530

    after home

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 import  express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import {checkHeader} from "./middleware/checkHeader.js";
 import checkTheMethod from "./middleware/onlyMethod.js";
-dotenv.config()
-const app = express();
-let PORT = process.env.PORT
 
-app.get("/",(req,res)=>{
-    res.write("home page")
-    res.end()
+dotenv.config();
+const app = express();
+let PORT = process.env.PORT;
+
+app.use((req,res,next)=>{
+    res.locals.user = "manu";
+    next();
 })
 
-// app.get("/",checkHeader,(req,res)=>{
-//     res.write("home page")
-//     res.end()
-// })
+app.get("/",(req,res)=>{
+ console.log(res.locals.user);
+    res.write("home page");
+    res.end();
+})
+
+app.get("/",checkHeader,(req,res)=>{
+    res.write("home page");
+    res.end();
+})
 
 app.use("/",checkTheMethod);
 app.listen(PORT,()=>{
-    console.log("the port running on the port :"+PORT)
+console.log("the port running on the port :"+PORT);
 })
